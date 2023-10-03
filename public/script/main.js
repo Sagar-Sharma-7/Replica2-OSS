94fdcbca8d11cc060806b0d12da8f2bcfcee5910
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-10-03 17:52:05 +0530

    header navbar buttons js and links smooth scrolling

--- public/script/main.js
+++ public/script/main.js
@@ -1,4 +1,12 @@
 const backToTop = document.querySelector(".back-to-top");
+const logoWrap = document.querySelector(".logo");
+
+// github link
+logoWrap.addEventListener("click", () => {
+    window.open("https://github.com/Sagar-Sharma-7/Replica2-OSS");
+})
+
+// back to top button script
 window.onscroll = function() {
     scrollBackToTop();
 }
