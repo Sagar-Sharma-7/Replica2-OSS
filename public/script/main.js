04ac38883cd92960ed90aad81be5f09adf59ae42
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2023-10-03 17:58:26 +0530

    added team members information

--- public/script/main.js
+++ public/script/main.js
@@ -1,5 +1,23 @@
-const backToTop = document.querySelector(".back-to-top");
-const logoWrap = document.querySelector(".logo");
+const backToTop = document.querySelector(".back-to-top"),
+      logoWrap = document.querySelector(".logo"),
+      info_box = document.querySelector(".info_box"),
+      cross_btn = document.querySelector("#cross_info_btn");
+
+
+// timer to show info box
+const timer = () => {
+    setTimeout(() => {
+        info_box.style.display = "flex";
+    }, 1000);
+};
+
+// closing the info box
+cross_btn.addEventListener("click", () => {
+    info_box.style.display = "none";
+});
+
+timer();
+
 
 // github link
 logoWrap.addEventListener("click", () => {
